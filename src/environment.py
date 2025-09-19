54dbfb04fba178cbcb34507b61a3d323077ccd00
AiroPi <47398145+AiroPi@users.noreply.github.com>
2025-09-20 00:42:14 +0200

    feat!: set code-default variables to non-docker env

--- src/environment.py
+++ src/environment.py
@@ -1,9 +1,9 @@
 from os import environ
+from pathlib import Path
 
-FLARESOLVERR_HOST = environ.get("FLARESOLVERR_HOST") or "flaresolverr"
+FLARESOLVERR_HOST = environ.get("FLARESOLVERR_HOST") or "localhost"
 FLARESOLVERR_PORT = environ.get("FLARESOLVERR_PORT") or 8191
 
-LIBRARY_PATH = environ.get("LIBRARY_PATH") or "/data/library"
-DOWNLOADS_PATH = environ.get("DOWNLOADS_PATH") or "/data/downloads"
+LIBRARY_PATH = Path(environ.get("LIBRARY_PATH") or "./data/library")
 
-DATABASE_PATH = environ.get("DATABASE_PATH") or "/data/db.sqlite"
+DATABASE_PATH = environ.get("DATABASE_PATH") or "./data/db.sqlite"
