ed86594159fe5b3c95b2defdf03283f32943f924
AiroPi <47398145+AiroPi@users.noreply.github.com>
2025-09-20 02:47:33 +0200

    feat: add notifications and uniform notify message

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ from media_sources.mangamoins import (
     download_chapter as mangamoins_download_chapter,
 )
 from media_sources.mangaplus import Chapter as MangaPlusChapter, MangaPlusSource
-from notifier import notify
+from notifier import notify_new_chapter
 
 media = MediaSub(
     db_path=DATABASE_PATH,
@@ -21,9 +21,18 @@ media = MediaSub(
 
 @media.sub_to(FMTeamSource())
 async def on_fmteam_chapter(src: FMTeamSource, chapter: FMTeamChapter):
+    pings = {
+        "Berserk": "1391138266685636679",
+        "Kingdom": "1391138396872773632",
+        "Vinland Saga": "1418756769718599740",
+    }
+
     print(f"New chapter available: {chapter.manga} - {chapter.chapter} ({chapter.title})")
-    await notify(
-        f"Nouveau chapitre de {chapter.manga} : {chapter.chapter} - {chapter.title}\nLisez-le sur https://fmteam.fr"
+    await notify_new_chapter(
+        manga=chapter.manga,
+        chapter=f"{chapter.chapter} - {chapter.title}",
+        url="https://fmteam.fr/",
+        ping=[pings[chapter.manga]],
     )
 
     path = Path("./library") / chapter.manga / f"c{chapter.chapter}.cbz"
@@ -37,10 +46,18 @@ async def on_fmteam_chapter(src: FMTeamSource, chapter: FMTeamChapter):
 
 @media.sub_to(MangaMoinsSource())
 async def on_mangamoins_chapter(src: MangaMoinsSource, chapter: MangaMoinsChapter):
+    pings = {
+        "OP": "1391138368838172682",
+    }
+
     print(f"New chapter available: {chapter.manga} - {chapter.chapter}")
-    await notify(
-        f"Nouveau chapitre de {chapter.manga} : {chapter.chapter}\nLisez-le sur https://mangamoins.shaeishu.co/"
+    await notify_new_chapter(
+        manga=chapter.manga,
+        chapter=chapter.chapter,
+        url=f"https://mangamoins.shaeishu.co/?scan={chapter.code}{chapter.chapter}",
+        ping=[pings[chapter.code]],
     )
+
     path = LIBRARY_PATH / chapter.manga / f"c{chapter.chapter}.cbz"
 
     if path.exists():
@@ -65,9 +82,17 @@ async def on_mangamoins_chapter(src: MangaMoinsSource, chapter: MangaMoinsChapte
     )
 )
 async def on_mangaplus_chapter(src: MangaPlusSource, chapter: MangaPlusChapter):
+    pings = {
+        700014: "1418758598640668786",
+        700006: "1418756725594390658",
+    }
+
     print(f"New chapter available: {chapter.manga} - {chapter.title}")
-    await notify(
-        f"Nouveau chapitre de {chapter.manga} : {chapter.title}\nLisez-le sur https://mangaplus.shueisha.co.jp/"
+    await notify_new_chapter(
+        manga=chapter.manga,
+        chapter=chapter.title,
+        url=f"https://mangaplus.shueisha.co.jp/viewer/{chapter.chapter_id}",
+        ping=[pings[chapter.manga_id]],
     )
 
 
--- src/notifier.py
+++ src/notifier.py
@@ -5,6 +5,18 @@ from httpx import AsyncClient
 WEBHOOK_URL = os.environ["DISCORD_WEBHOOK_URL"]
 
 
+async def notify_new_chapter(manga: str, chapter: str, url: str, ping: list[str]):
+    message = (
+        "**Un nouveau chapitre est disponible !**\n\n"
+        f"__Manga__: {manga}\n"
+        f"__Chapitre__: {chapter}\n\n"
+        f"Lisez le dès à présent ici : <{url}>\n"
+        f"<@&{'> <@&'.join(['1418756697777901620', *ping])}"
+    )
+
+    await notify(message)
+
+
 async def notify(message: str) -> None:
     """Send a notification to Discord using Webhook"""
     async with AsyncClient() as client:
